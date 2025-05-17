c66cb306aa26b4ad4b978a109d8a1dfb25ed6403
YuxuanZeng <yuxuan.zeng@tum.de>
2025-05-17 11:56:09 +0200

    step02

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"os/exec"
 )
 import "os"
 import "bufio"
+import "github.com/danicat/simpleansi"
 
 var maze []string
 
@@ -25,12 +27,42 @@ func loadMaze(file string) error {
 }
 
 func printScreen() {
+	simpleansi.ClearScreen()
 	for _, line := range maze {
 		fmt.Println(line)
 	}
 }
+func initialize() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to activate the cbreak Modul", err)
+	}
+}
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to recover to cooked module", err)
+	}
+}
+func readinput() (string, error) {
+	buffer := make([]byte, 100)
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+	return "", nil
+}
 func main() {
 	// initialize game
+	initialize()
+	defer cleanup()
 
 	// load resources
 	err := loadMaze("maze01.txt")
@@ -44,6 +76,14 @@ func main() {
 		// update screen
 		printScreen()
 		// process input
+		input, err := readinput()
+		if err != nil {
+			log.Print("error reading input", err)
+			break
+		}
+		if input == "ESC" {
+			break
+		}
 
 		// process movement
 
